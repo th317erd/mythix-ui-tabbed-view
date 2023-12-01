392260e31d72f7f68a073b30bdca8d0a2b928730
Wyatt Greenway <th317erd@gmail.com>
2023-12-01 11:03:38 -0700

    Initial project structure setup

---
+++ spec/lib/lib-spec.js
@@ -0,0 +1,14 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-array-constructor */
+/* eslint-disable no-magic-numbers */
+
+import * as _TestHelpers from '../support/test-helpers.js';
+
+import Lib from '../../lib/index.js';
+
+describe('Lib', () => {
+  describe('something', () => {
+    it('works', () => {
+    });
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+import Path         from 'node:path';
+import TerserPlugin from 'terser-webpack-plugin';
+
+const isProduction = (process.env.NODE_ENV === 'production');
+
+const config = {
+  entry:    './lib/index.js',
+  devtool:  (isProduction) ? 'source-map' : 'inline-source-map',
+  mode:     'production',
+  output:   {
+    path:               Path.resolve('./dist'),
+    scriptType:         'module',
+    filename:           'index.js',
+    sourceMapFilename:  'index.js.map',
+    libraryTarget:      'module',
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+  },
+  experiments: {
+    outputModule: true,
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel:       true,
+        terserOptions:  {
+          keep_classnames:  false,
+          keep_fnames:      false,
+          ecma:             2015,
+          module:           true,
+        },
+      }),
+    ],
+  },
+};
+
+export default () => {
+  if (isProduction)
+    config.mode = 'production';
+  else
+    config.mode = 'development';
+
+  return config;
+};
